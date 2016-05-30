c9b4404bbf8430fd92982a12bbaed55ac494aa1d
Fatih Yelci <fatih.yelci@foreks.com>
2016-05-30 15:48:35 +0300

    Added search functionality

--- app/scripts/controllers/flight-search.controller.js
+++ app/scripts/controllers/flight-search.controller.js
@@ -15,74 +15,109 @@
   FlightSearchController.$inject = ['FlightSearchService'];
 
   function FlightSearchController (FlightSearchService) {
-      var vm = this;
-      var today = new Date();
-      var maxDate = new Date();
-      maxDate.setMonth(maxDate.getMonth() + 6);
+    var vm = this;
+    var today = new Date();
+    var maxDate = new Date();
+    maxDate.setMonth(maxDate.getMonth() + 6);
 
-      vm.init = function() {
-        vm.fromAirportList = [];
-        vm.toAirportList = [];
-        vm.flightResults = [];
-        vm.passengerSelectItems = [1,2,3,4,5,6,7,8,9,10];
+    vm.init = function() {
+      vm.fromAirportList = [];
+      vm.toAirportList = [];
+      vm.flightResults = [];
+      vm.passengerSelectItems = [1,2,3,4,5,6,7,8,9,10];
+      vm.alerts = [];
 
-        vm.filters = {
-          airports: {
-            from: undefined,
-            to: undefined
-          },
-          departure: {
-            datePopupOpened: false,
-            date: null
-          },
-          return: {
-            datePopupOpened: false,
-            date: null
-          },
-          passengerCount: null,
-          priceRangeStart: 0,
-          priceRangeEnd: 200
-        };
-
-        FlightSearchService.getFromAirportList(function(responseData) {
-          vm.fromAirportList = responseData;
-        });
-
-        vm.departureDateOptions = {
-          formatYear: 'yy',
-          maxDate: maxDate,
-          minDate: today,
-          startingDay: 1
-        };
-        vm.returnDateOptions = {
-          formatYear: 'yy',
-          maxDate: maxDate,
-          minDate: today,
-          startingDay: 1
-        };
-        vm.dateFormats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-        vm.dateFormat = vm.dateFormats[0];
+      vm.filters = {
+        airports: {
+          from: undefined,
+          to: undefined
+        },
+        departure: {
+          datePopupOpened: false,
+          date: null
+        },
+        return: {
+          datePopupOpened: false,
+          date: null
+        },
+        passengerCount: null,
+        priceRangeStart: 0,
+        priceRangeEnd: 200,
+        isRoundTrip: true
       };
 
-      vm.openDepartureDatePopup = function() {
-        vm.filters.departure.datePopupOpened=true;
-      };
-      vm.openReturnDatePopup = function() {
-        vm.filters.return.datePopupOpened=true;
-      };
+      FlightSearchService.getFromAirportList(function(responseData) {
+        vm.fromAirportList = responseData;
+      });
 
-      vm.fromAirportSelected = function() {
-        FlightSearchService.getToAirportList(vm.filters.airports.from, function(responseData) {
-          vm.toAirportList = responseData;
-        });
+      vm.departureDateOptions = {
+        formatYear: 'yy',
+        maxDate: maxDate,
+        minDate: today,
+        startingDay: 1
       };
-
-      vm.searchFlights = function() {
-        FlightSearchService.searchFlights(vm.filters, function(responseData) {
-          vm.flightResults = responseData;
-        });
+      vm.returnDateOptions = {
+        formatYear: 'yy',
+        maxDate: maxDate,
+        minDate: today,
+        startingDay: 1
       };
+      vm.dateFormats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+      vm.dateFormat = vm.dateFormats[0];
+    };
 
-      vm.init();
+    vm.openDepartureDatePopup = function() {
+      vm.filters.departure.datePopupOpened=true;
+    };
+    vm.openReturnDatePopup = function() {
+      vm.filters.return.datePopupOpened=true;
+    };
+
+    vm.fromAirportSelected = function() {
+      FlightSearchService.getToAirportList(vm.filters.airports.from, function(responseData) {
+        vm.toAirportList = responseData;
+      });
+    };
+
+    vm.searchFlights = function() {
+      vm.alerts = [];
+      filterValidation();
+      if(vm.alerts.length > 0) {
+        return;
+      }
+
+      FlightSearchService.searchFlights(vm.filters, function(responseData) {
+        vm.flightResults = responseData;
+      });
+    };
+
+    function filterValidation () {
+      if(!vm.filters.airports.from) {
+        vm.alerts.push({type: 'warning', msg: 'Departure airport is mandatory for flight search!'});
+      }
+      if(!vm.filters.airports.to) {
+        vm.alerts.push({type: 'warning', msg: 'Arrival airport is mandatory for flight search!'});
+      }
+      if(!vm.filters.departure.date) {
+        vm.alerts.push({type: 'warning', msg: 'Departure date is mandatory for flight search!'});
+      }
+      if(!vm.filters.passengerCount) {
+        vm.alerts.push({type: 'warning', msg: 'You should choose passenger count!'});
+      }
+      if(vm.filters.isRoundTrip && !vm.filters.return.date) {
+        vm.alerts.push({type: 'warning', msg: 'You should choose return date for return flight!'});
+      }
+    };
+
+    vm.selectUnselectFlight = function (index) {
+      vm.flightResults[index].isSelected =!vm.flightResults[index].isSelected;
+    };
+
+    vm.directionSelected = function(isRoundTrip) {
+      vm.isRoundTrip = isRoundTrip;
+    };
+
+
+    vm.init();
   }
 })();
--- app/scripts/services/flight-search.service.js
+++ app/scripts/services/flight-search.service.js
@@ -1,75 +1,94 @@
 (function() {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('flightSearchEngineApp')
-        .factory('FlightSearchService', FlightSearchService);
+  angular
+      .module('flightSearchEngineApp')
+      .factory('FlightSearchService', FlightSearchService);
 
-    FlightSearchService.$inject = ['$http', '_'];
-    function FlightSearchService ($http, _) {
-        function getFromAirportList(callbackFunc) {
-          return $http.get('data/flights.json').success(function(data) {
-              var airportData = [];
-              for (var i=0; i<data.flights.length; i++) {
-                if(_.findIndex(airportData, {code : data.flights[i].fromAirportCode}) < 0) {
-                  airportData.push({
-                    code: data.flights[i].fromAirportCode,
-                    name: data.flights[i].fromAirport,
-                    city: data.flights[i].fromAirportCity
-                  });
-                }
-              }
-            callbackFunc(airportData);
-          });
-        }
-
-        function getToAirportList(fromAirport ,callbackFunc) {
-          return $http.get('data/flights.json').success(function(data) {
-              var airportData = [];
-              for (var i=0; i<data.flights.length; i++) {
-                if(fromAirport.code === data.flights[i].fromAirportCode
-                    && _.findIndex(airportData, {code : data.flights[i].toAirportCode}) < 0) {
-                  airportData.push({
-                    code: data.flights[i].toAirportCode,
-                    name: data.flights[i].toAirport,
-                    city: data.flights[i].toAirportCity
-                  });
-                }
-              }
-            callbackFunc(airportData);
-          });
-        }
-
-        function searchFlights(filters, callbackFunc) {
-          return $http.get('data/flights.json').success(function(data) {
-              var flightData = [];
-              for (var i=0; i<data.flights.length; i++) {
-                var departureDate = new Date(data.flights[i].departureDate);
-                var arrivalDate = new Date(data.flights[i].arrivalDate);
-                if(data.flights[i].fromAirportCode == filters.airports.from.code
-                   && data.flights[i].toAirportCode == filters.airports.to.code
-                   && departureDate.sameDay(filters.departure.date)
-                   && filters.passengerCount <= data.flights[i].remainingTickets) {
-                     flightData.push({
-                       departureFlight: getConvertedObj(data.flights[i]),
-                       returnFlight: getConvertedObj(data.flights[0])
-                     });
-                }
-              }
-            callbackFunc(flightData);
-          });
-        }
-
-        function getConvertedObj (flight) {
-          flight.departureDate = new Date(flight.departureDate);
-          flight.arrivalDate = new Date(flight.arrivalDate);
-          return flight;
-        }
-
-        return {
-          getFromAirportList: getFromAirportList,
-          getToAirportList: getToAirportList,
-          searchFlights: searchFlights
-        };
+  FlightSearchService.$inject = ['$http', '_'];
+  function FlightSearchService ($http, _) {
+    function getFromAirportList(callbackFunc) {
+      return $http.get('data/flights.json').success(function(data) {
+          var airportData = [];
+          for (var i=0; i<data.flights.length; i++) {
+            if(_.findIndex(airportData, {code : data.flights[i].fromAirportCode}) < 0) {
+              airportData.push({
+                code: data.flights[i].fromAirportCode,
+                name: data.flights[i].fromAirport,
+                city: data.flights[i].fromAirportCity
+              });
+            }
+          }
+        callbackFunc(airportData);
+      });
     }
+
+    function getToAirportList(fromAirport ,callbackFunc) {
+      return $http.get('data/flights.json').success(function(data) {
+          var airportData = [];
+          for (var i=0; i<data.flights.length; i++) {
+            if(fromAirport.code === data.flights[i].fromAirportCode
+                && _.findIndex(airportData, {code : data.flights[i].toAirportCode}) < 0) {
+              airportData.push({
+                code: data.flights[i].toAirportCode,
+                name: data.flights[i].toAirport,
+                city: data.flights[i].toAirportCity
+              });
+            }
+          }
+        callbackFunc(airportData);
+      });
+    }
+
+    function searchFlights(filters, callbackFunc) {
+      var successCallback = function(data) {
+        var flightData = [];
+        //Check if departure flight is elgible
+        for (var i=0; i<data.flights.length; i++) {
+          var departureDate = new Date(data.flights[i].departureDate);
+
+          if(data.flights[i].fromAirportCode === filters.airports.from.code
+              && data.flights[i].toAirportCode === filters.airports.to.code
+              && departureDate.sameDay(filters.departure.date)
+              && filters.passengerCount <= data.flights[i].remainingTickets) {
+            flightData.push({
+              departureFlight: getConvertedObj(data.flights[i])
+            });
+          }
+        }
+
+        //Check if return flight is eligible
+        if(flightData.length > 0) {
+          for (var i=0; i<data.flights.length; i++) {
+            var departureDate = new Date(data.flights[i].departureDate);
+
+            if(data.flights[i].toAirportCode === filters.airports.from.code
+              && data.flights[i].fromAirportCode === filters.airports.to.code
+              && departureDate.sameDay(filters.return.date)
+              && filters.passengerCount <= data.flights[i].remainingTickets) {
+              for(var j=0; j < flightData.length; j++) {
+                flightData[j].returnFlight = getConvertedObj(data.flights[i]);
+              }
+            }
+          }
+        }
+
+        callbackFunc(flightData);
+      };
+
+      return $http.get('data/flights.json').success(successCallback);
+    }
+
+    function getConvertedObj (flight) {
+      flight.departureDate = new Date(flight.departureDate);
+      flight.arrivalDate = new Date(flight.arrivalDate);
+      return flight;
+    }
+
+    return {
+      getFromAirportList: getFromAirportList,
+      getToAirportList: getToAirportList,
+      searchFlights: searchFlights
+    };
+  }
 })();
